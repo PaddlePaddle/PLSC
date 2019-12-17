fd4283572661dc75ee1608c271f34ab7b8df9cc4
sandyhouse <lilong12@baidu.com>
2019-12-17 11:37:30 +0800

    add logging level

--- plsc/entry.py
+++ plsc/entry.py
@@ -44,6 +44,7 @@ from paddle.fluid.optimizer import Optimizer
 
 
 logging.basicConfig(
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%d %b %Y %H:%M:%S')
 logger = logging.getLogger(__name__)
