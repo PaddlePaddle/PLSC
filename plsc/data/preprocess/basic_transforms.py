f90bc14b3297fba94344f8e77203c032c241c7eb
GuoxiaWang <mingzilaochongtu@gmail.com>
2022-11-18 17:12:26 +0800

    add compose in basic_transforms and fix norm callable in vision_transformer

--- plsc/data/preprocess/basic_transforms.py
+++ plsc/data/preprocess/basic_transforms.py
@@ -34,6 +34,24 @@ class OperatorParamError(ValueError):
     pass
 
 
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        for t in self.transforms:
+            img = t(img)
+        return img
+
+    def __repr__(self) -> str:
+        format_string = self.__class__.__name__ + "("
+        for t in self.transforms:
+            format_string += "\n"
+            format_string += f"    {t}"
+        format_string += "\n)"
+        return format_string
+
+
 class DecodeImage(object):
     """ decode image """
 
