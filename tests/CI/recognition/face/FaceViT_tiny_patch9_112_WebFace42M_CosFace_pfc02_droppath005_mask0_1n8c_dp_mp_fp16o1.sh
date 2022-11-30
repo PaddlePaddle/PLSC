2d8faf927dbf2c3969397c384db245fab6667bc5
Guoxia Wang <mingzilaochongtu@gmail.com>
2022-11-30 18:16:06 +0800

    add face recognition ci (#150)

---
+++ tests/CI/recognition/face/FaceViT_tiny_patch9_112_WebFace42M_CosFace_pfc02_droppath005_mask0_1n8c_dp_mp_fp16o1.sh
@@ -0,0 +1,33 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export PADDLE_NNODES=1
+export PADDLE_MASTER="127.0.0.1:12538"
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+python -m paddle.distributed.launch \
+    --nnodes=$PADDLE_NNODES \
+    --master=$PADDLE_MASTER \
+    --devices=$CUDA_VISIBLE_DEVICES \
+    plsc-train \
+    -c ../../task/recognition/face/configs/FaceViT_tiny_patch9_112_WebFace42M_CosFace_pfc02_droppath005_mask0_1n8c_dp_mp_fp16o1.yaml \
+    -o Global.print_batch_step=1 \
+    -o Global.max_train_step=200 \
+    -o Global.flags.FLAGS_cudnn_exhaustive_search=0 \
+    -o Global.flags.FLAGS_cudnn_deterministic=1 \
+    -o Model.class_num=93431 \
+    -o DataLoader.Train.dataset.image_root=./dataset/MS1M_v3/ \
+    -o DataLoader.Train.dataset.cls_label_path=./dataset/MS1M_v3/label.txt \
+    -o DataLoader.Eval.dataset.image_root=./dataset/MS1M_v3/agedb_30/ \
+    -o DataLoader.Eval.dataset.cls_label_path=./dataset/MS1M_v3/agedb_30/label.txt \
