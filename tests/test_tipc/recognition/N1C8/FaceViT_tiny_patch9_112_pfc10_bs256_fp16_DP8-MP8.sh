78f36066fb154c8a6b7001e7b0012035ba33ccb6
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2022-12-14 16:05:08 +0800

    update benchmark scripts (#159)

--- tests/test_tipc/recognition/N1C8/FaceViT_tiny_patch9_112_pfc10_bs256_fp16_DP8-MP8.sh
+++ tests/test_tipc/recognition/N1C8/FaceViT_tiny_patch9_112_pfc10_bs256_fp16_DP8-MP8.sh
@@ -12,14 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-model_item=IResNet50_ratio10
+model_item=FaceViT_tiny_patch9_112_pfc10
 fp_item=fp16
-bs_item=128
-run_mode=DP8_MP1
+bs_item=256
+run_mode=DP8-MP8
 device_num=N1C8
-yaml_path=./plsc/configs/FaceRecognition/IResNet50_MS1MV3_ArcFace_1.0_1n8c_dp_fp16o2.yaml
-epochs=20
+yaml_path=./task/recognition/face/configs/FaceViT_tiny_patch9_112_WebFace42M_CosFace_pfc10_droppath005_mask0_1n8c_dp_mp_fp16o1.yaml \
+max_iter=600
+sample_ratio=1.0
+model_parallel=True
 
 bash ./tests/test_tipc/recognition/benchmark_common/prepare.sh
 # run
-bash ./tests/test_tipc/recognition/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${bs_item} ${run_mode} ${device_num} ${yaml_path} ${epochs} 2>&1;
+bash ./tests/test_tipc/recognition/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${bs_item} ${run_mode} ${device_num} ${yaml_path} \
+${max_iter} ${sample_ratio} ${model_parallel} 2>&1;
--- tests/test_tipc/recognition/N1C8/IResNet50_pfc01_bs128_fp16_DP8-MP1.sh
+++ tests/test_tipc/recognition/N1C8/IResNet50_pfc01_bs128_fp16_DP8-MP1.sh
@@ -12,14 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-model_item=IResNet50_ratio10
+model_item=IResNet50_pfc01
 fp_item=fp16
 bs_item=128
-run_mode=DP1_MP1
-device_num=N1C1
-yaml_path=./plsc/configs/FaceRecognition/IResNet50_MS1MV3_ArcFace_1.0_1n8c_dp_fp16o2.yaml
-epochs=2
+run_mode=DP8-MP1
+device_num=N1C8
+yaml_path=./task/recognition/face/configs/IResNet50_MS1MV3_ArcFace_pfc10_1n8c_dp_mp_fp16o1.yaml \
+max_iter=600
+sample_ratio=0.1
+model_parallel=False
 
 bash ./tests/test_tipc/recognition/benchmark_common/prepare.sh
 # run
-bash ./tests/test_tipc/recognition/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${bs_item} ${run_mode} ${device_num} ${yaml_path} ${epochs} 2>&1;
+bash ./tests/test_tipc/recognition/benchmark_common/run_benchmark.sh ${model_item} ${fp_item} ${bs_item} ${run_mode} ${device_num} ${yaml_path} \
+${max_iter} ${sample_ratio} ${model_parallel} 2>&1;
