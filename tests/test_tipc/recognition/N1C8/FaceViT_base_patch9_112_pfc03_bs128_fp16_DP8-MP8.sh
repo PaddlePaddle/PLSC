0214ee9bb31adb06af334c20f93994ef5b4ec112
Liujie0926 <44688141+Liujie0926@users.noreply.github.com>
2023-03-02 14:18:37 +0800

    [Benchmark]Update step&add new case (#204)

--- tests/test_tipc/recognition/N1C8/FaceViT_base_patch9_112_pfc03_bs128_fp16_DP8-MP8.sh
+++ tests/test_tipc/recognition/N1C8/FaceViT_base_patch9_112_pfc03_bs128_fp16_DP8-MP8.sh
@@ -1,11 +1,11 @@
 # Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,7 @@ bs_item=128
 run_mode=DP8-MP8
 device_num=N1C8
 yaml_path=./task/recognition/face/configs/FaceViT_base_patch9_112_WebFace42M_CosFace_pfc03_droppath005_mask005_1n8c_dp_mp_fp16o1.yaml \
-max_iter=600
+max_iter=10117 # epoch=2
 sample_ratio=0.3
 model_parallel=True
 
