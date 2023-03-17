1d5f78606d0175daf9860fac087ac8f88ca14a96
liujie44 <liujie44@baidu.com>
2023-03-17 14:28:24 +0800

    update ConvNeXt_base_224_in1k_1n8c_dp_fp32 scripts

--- tests/CI/case.sh
+++ tests/CI/case.sh
@@ -241,7 +241,6 @@ function ConvNeXt_base_224_in1k_1n8c_dp_fp32() {
     cd ${plsc_path}
     rm -rf log
     bash ./classification/convnext/ConvNeXt_base_224_in1k_1n8c_dp_fp32.sh
-    check_result $FUNCNAME
     loss=`tail log/workerlog.0 | grep "50/313" | cut -d " " -f12 `
     ips=`cat log/workerlog.0 |grep ips: |cut -d " " -f18 |awk 'NR>1 {print}' | awk '{a+=$1}END{print a/NR}'`
     check_result 6.91451 ${loss%?} 706.8508 ${ips} $FUNCNAME
